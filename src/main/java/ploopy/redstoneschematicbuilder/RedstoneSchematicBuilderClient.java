e374e53edd959d74935046600b56ed182475d647
JacobChan182 <jacobchanemail@gmail.com>
2025-10-19 02:15:58 -0400

    Initial commit

---
+++ src/main/java/ploopy/redstoneschematicbuilder/RedstoneSchematicBuilderClient.java
@@ -0,0 +1,10 @@
+package ploopy.redstoneschematicbuilder;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RedstoneSchematicBuilderClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
